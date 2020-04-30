860ab6636d1ce2f234e63315a2d08a12e51989d3
REV3NT3CH <rev3nt3ch@gmail.com>
2020-04-30 02:59:14 -0700

    Upstream to Android Q base

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -37,7 +37,7 @@ except ImportError:
 # set this to the default remote to use in repo
 default_rem = "black"
 # set this to the default revision to use (branch/tag name)
-default_rev = "n"
+default_rev = "q"
 # set this to the remote that you use for projects from your team repos
 # example fetch="https://github.com/omnirom"
 default_team_rem = "gh"
